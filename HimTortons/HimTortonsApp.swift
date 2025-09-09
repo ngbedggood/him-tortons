cf9018eccbf633c3634d272b269da7f2060b3b78
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-09-09 11:17:28 -0700

    Initial commit

---
+++ HimTortons/HimTortonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HimTortonsApp.swift
+//  HimTortons
+//
+//  Created by Nathaniel Bedggood on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct HimTortonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
