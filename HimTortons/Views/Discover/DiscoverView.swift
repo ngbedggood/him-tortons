fcbcbbe2490ae62ccd6ab96388174351f06c609d
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-09-09 13:25:56 -0700

    discovery scroll view tweak

--- HimTortons/Views/Discover/DiscoverView.swift
+++ HimTortons/Views/Discover/DiscoverView.swift
@@ -9,23 +9,25 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        ScrollView{
-            HStack {
-                Text("Explore what's new")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                Spacer()
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Explore what's new")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                ForEach(0..<4) { _ in
+                    DiscoverCardView(
+                        imageUrl: "https://i.imgflip.com/9e4vwk.jpg",
+                        title: "Scan to Win is on!",
+                        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                        buttonType: "Scan now"
+                    )
+                }
             }
-            ForEach(0..<4) { _ in
-                DiscoverCardView(
-                    imageUrl: "https://i.imgflip.com/9e4vwk.jpg",
-                    title: "Scan to Win is on!",
-                    caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    buttonType: "Scan now"
-                )
-            }
+            .padding()
         }
-        .padding()
         .background(.tan)
     }
 }
