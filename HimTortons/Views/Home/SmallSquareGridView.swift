f623d7f34423a8a5e1b2a3b292ce7d74159109cb
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-09-09 17:06:38 -0700

    small square grid

---
+++ HimTortons/Views/Home/SmallSquareGridView.swift
@@ -0,0 +1,71 @@
+//
+//  BigSquareGridView.swift
+//  HimTortons
+//
+//  Created by Nathaniel Bedggood on 09/09/2025.
+//
+
+struct smallSquareItem: Identifiable{
+    let id: UUID = UUID()
+    let icon: String
+    let itemName: String
+}
+
+import SwiftUI
+
+struct SmallSquareGridView: View {
+    
+    let items: [smallSquareItem] = [
+        smallSquareItem(icon: "creditcard.fill", itemName: "Hims Financial"),
+        smallSquareItem(icon: "tag.fill", itemName: "Offers"),
+        smallSquareItem(icon: "moped.fill", itemName: "Delivery"),
+        smallSquareItem(icon: "bag.fill", itemName: "HimShop"),
+        smallSquareItem(icon: "heart.fill", itemName: "Community"),
+        smallSquareItem(icon: "mail.and.text.magnifyingglass", itemName: "Hims Word Challenge"),
+        smallSquareItem(icon: "gift.fill", itemName: "Give"),
+        smallSquareItem(icon: "mappin.and.ellipse", itemName: "Restaurant Locator"),
+        smallSquareItem(icon: "person.2.fill", itemName: "Join our team")
+    ]
+    
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 8) {
+            ForEach(items) { item in
+                VStack{
+                    VStack(alignment: .center) {
+                        Image(systemName: item.icon)
+                            .resizable()
+                            .scaledToFit()
+                            .padding(8)
+                            .foregroundColor(.teal)
+                            .frame(width: 50, height: 50)
+                        
+                    }
+                    .padding(8)
+                    .background(.white)
+                    .cornerRadius(16)
+                    .aspectRatio(1, contentMode: .fit)
+                    .shadow(color: Color.black.opacity(0.05), radius: 32)
+                    
+                    Text(item.itemName)
+                        .font(.caption)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+            }
+            
+        }
+        .multilineTextAlignment(.center)
+        .padding()
+    }
+}
+
+#Preview {
+    SmallSquareGridView()
+}
