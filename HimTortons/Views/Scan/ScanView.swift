5af233c07fd346b6577b999ff04f377d9ba0e680
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-09-25 10:40:32 -0700

    added scanview

---
+++ HimTortons/Views/Scan/ScanView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanView.swift
+//  HimTortons
+//
+//  Created by Nathaniel Bedggood on 25/09/2025.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanView()
+}
